6ba39959b4b7d79df45c1df32c10f825139fccf8
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-11-07 16:04:56 +0200

    created blogs collection and added get and post method for add blogs

--- index.js
+++ index.js
@@ -33,12 +33,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const blogCollection = client.db('blogsDB').collection("blogs");
+
+    app.get('/addBlog', async (req, res) => {
+      const cursor = blogCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.post('/addBlog', async (req, res) => {
+      const newBlog = req.body;
+      console.log(newBlog);
+      const result = await blogCollection.insertOne(newBlog);
+      res.send(result); 
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
