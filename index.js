7149d9661918fbf79b18fe9a923bca183fedbfdc
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-11-08 22:10:41 +0200

    added get method for blogDetails

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,15 @@ async function run() {
       const cursor = blogCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+//View details Blog
+    app.get('/BlogDetails/:_id', async (req, res) => {
+      const blogID = req.params._id;
+      const idObject = new ObjectId (blogID)
+      const result = await blogCollection.findOne(idObject);
+      console.log(result);
+      res.send(result); 
     })
 
 
