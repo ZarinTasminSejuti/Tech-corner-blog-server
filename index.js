4023b7b5c0281f89d7c71d36f5c04301bd4c685c
Zarin Tasmin Sejuti <zts-7234@hotmail.com>
2023-11-09 23:57:54 +0200

    modified get addBlog to /allBlog

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // // Middleware
-const corsOptions ={
-  origin:'*', 
-  credentials:true,
-  optionSuccessStatus:200,
-}
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.ggegvme.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,77 +31,135 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const blogCollection = client.db('blogsDB').collection("blogs");
-    const commentCollection = client.db('blogsDB').collection("comment");
-    const suggestionCollection = client.db('blogsDB').collection("suggestion");
+    const blogCollection = client.db("blogsDB").collection("blogs");
+    const commentCollection = client.db("blogsDB").collection("comment");
+    const suggestionCollection = client.db("blogsDB").collection("suggestion");
+    const wishlistCollection = client.db("blogsDB").collection("wishlist");
 
-    app.get('/addBlog', async (req, res) => {
+    app.get("/allBlog", async (req, res) => {
       const cursor = blogCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-//View details Blog
-    app.get('/BlogDetails/:_id', async (req, res) => {
+    //View details Blog
+    app.get("/BlogDetails/:_id", async (req, res) => {
       const blogID = req.params._id;
-      const idObject = new ObjectId (blogID)
+      const idObject = new ObjectId(blogID);
       const result = await blogCollection.findOne(idObject);
-      res.send(result); 
-    })
+      res.send(result);
+    });
 
     //Get specific blog comment
-    app.get('/comment', async (req, res) => {
+    app.get("/comment", async (req, res) => {
       const commentFound = commentCollection.find();
       const result = await commentFound.toArray();
       res.send(result);
-    })
+    });
 
-
-    app.post('/addBlog', async (req, res) => {
+    app.post("/addBlog", async (req, res) => {
       const newBlog = req.body;
       const result = await blogCollection.insertOne(newBlog);
+      res.send(result);
+    });
+
+  
+
+    //update section
+    app.get("/updateBlog/:_id", async (req, res) => {
+      const updateBlogId = req.params._id;
+      const idObject = new ObjectId(updateBlogId);
+      const result = await blogCollection.findOne(idObject);
+      res.send(result);
+      console.log(idObject);
+    });
+
+    app.put('/updateBlog/:_id', async (req, res) => {
+      const updateId = req.params._id;
+      const updateBlog = req.body;
+      console.log(updateBlog);
+      const filter = {_id: new ObjectId(updateId)}
+      const options = { upsert: true };
+      const updateBlogElement = {
+        $set: {
+          blogTitle: updateBlog.blogTitle,
+          type: updateBlog.type,
+          shortDescription: updateBlog.shortDescription,
+          longDescription: updateBlog.longDescription,
+          image: updateBlog.image,
+          userEmail: updateBlog.userEmail
+        }
+      }
+
+      const result = await blogCollection.updateOne(filter, updateBlogElement, options)
       res.send(result); 
+      console.log(result);
     })
 
 
-     //for comment form
-  app.post('/comment', async (req, res) => {
-    const newComment = req.body;
-    const result = await commentCollection.insertOne(newComment);
-    res.send(result); 
-  })
 
-  
-  //for suggestion form
-  app.post('/suggestion', async (req, res) => {
-    const newSuggestion = req.body;
-    const result = await suggestionCollection.insertOne(newSuggestion);
-    res.send(result); 
-  })
+    //wishlist section
+    app.get("/wishlist", async (req, res) => {
+      const pointer = wishlistCollection.find();
+      const result = await pointer.toArray();
+      res.send(result);
+    });
 
-    
-    
-    
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} finally {
-  // Ensures that the client will close when you finish/error
-  // await client.close();
-}
+    app.post("/wishlist", async (req, res) => {
+      const newWishlist = req.body;
+      console.log(newWishlist);
+      const result = await wishlistCollection.insertOne(newWishlist);
+      res.send(result);
+    });
+
+    //Remove wishlist section
+    app.delete("/wishlist/:listId", async (req, res) => {
+      const listId = req.params.listId;
+      const idObject = new ObjectId(listId);
+      console.log(listId);
+      const result = await wishlistCollection.deleteOne({ _id: idObject });
+      res.send(result);
+    });
+
+
+
+
+    //for comment form
+    app.post("/comment", async (req, res) => {
+      const newComment = req.body;
+      const result = await commentCollection.insertOne(newComment);
+      res.send(result);
+    });
+
+    //for suggestion form
+    app.post("/suggestion", async (req, res) => {
+      const newSuggestion = req.body;
+      const result = await suggestionCollection.insertOne(newSuggestion);
+      res.send(result);
+    });
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Blog server is running');
+app.get("/", (req, res) => {
+  res.send("Blog server is running");
 });
 
 app.listen(port, () => {
-    console.log(`Blog server is running on port: ${port}`);
+  console.log(`Blog server is running on port: ${port}`);
 });
